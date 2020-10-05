d410e17658b33d29b3e37a481f8094be1dae625a
waleoxy <waleoxy@gmail.com>
2020-10-05 14:03:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1>Hello from Olawale Otubu Web</h1>
+    )
+  }
+}
